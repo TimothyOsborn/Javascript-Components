a4139691365beffb86a8088a92503d2ea75f4b6b
Timothy Tomazi-Osborn <timothytomaziosborn@gmail.com>
2020-03-06 19:14:21 +1100

    bootstrap(typescript): added typescript and tsconfig with initial configuration

--- src/components/CardOptIcon/index.js
+++
@@ -1,9 +0,0 @@
-const CardWithIcon = () => {
-  return `<h1>Hello World</h1>`;
-};
-
-CardWithIcon.refs = {};
-
-document.addEventListener("DOMContentLoaded", () => {
-  new CardWithIcon(document.getElementById("CardWithIcon"));
-});
