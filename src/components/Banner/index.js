5297813e3a5bb340852e175a8be263e800ff1f0f
Timothy Tomazi-Osborn <timothytomaziosborn@gmail.com>
2020-03-11 22:22:43 +1100

    Added testing code for banner and played around with attributes

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,9 +1,29 @@
 class BannerElement extends HTMLElement {
+    constructor() {
+        super();
+        setTimeout(() => {
+            this.initialHTML = this.innerHTML;
+            this.initialText = this.innerText;
+        });
+    }
+    static get observedAttributes() {
+        return ['cat'];
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log('in attributechangedcallback', name, oldValue, newValue);
+        if (name === 'cat' && oldValue !== null && oldValue !== newValue) {
+            console.log('checking before clearing', this.innerHTML, this.innerText);
+            this.innerHTML = '';
+            this.connectedCallback();
+            console.log('checking after clearing', this.innerHTML, this.innerText);
+        }
+    }
     connectedCallback() {
         setTimeout(() => (this.innerHTML = `
         <div>
-          <h2>${this.innerText}</h2>
-          <h1>${this.innerHTML}</h1>
+          <h3>${this.getAttribute('cat')}</h3>
+          <h2>${this.initialText}</h2>
+          <h1>${this.initialHTML}</h1>
         </div>
         `));
     }
--- src/components/Banner/index.ts
+++ src/components/Banner/index.ts
@@ -1,13 +1,36 @@
 class BannerElement extends HTMLElement {
-  heading: string;
+  initialHTML: string;
+  initialText: string;
+  static get observedAttributes() {
+    return ['cat'];
+  }
+
+  constructor() {
+    super();
+    setTimeout(() => {
+      this.initialHTML = this.innerHTML;
+      this.initialText = this.innerText;
+    });
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    console.log('in attributechangedcallback', name, oldValue, newValue);
+    if (name === 'cat' && oldValue !== null && oldValue !== newValue) {
+      console.log('checking before clearing', this.innerHTML, this.innerText);
+      this.innerHTML = '';
+      this.connectedCallback();
+      console.log('checking after clearing', this.innerHTML, this.innerText);
+    }
+  }
 
   connectedCallback() {
     setTimeout(
       () =>
         (this.innerHTML = `
         <div>
-          <h2>${this.innerText}</h2>
-          <h1>${this.innerHTML}</h1>
+          <h3>${this.getAttribute('cat')}</h3>
+          <h2>${this.initialText}</h2>
+          <h1>${this.initialHTML}</h1>
         </div>
         `),
     );
