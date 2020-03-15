cad2b5b0dbe0db1ac48d620db4326073d327e5f1
Timothy Tomazi-Osborn <timothytomaziosborn@gmail.com>
2020-03-15 21:39:11 +1100

    chore(webpack): updated webpack to work with extracted css

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@ const homepage = () => {
   const element = document.createElement('div');
   element.classList.add('testing-class');
   element.innerHTML = `
-    <h1>Using JS Entry Point</h1>
-    <h3>${_.join(['Hello', 'World'], ' ')}
+    <div class="container">
+      <h1>Using JS Entry Point</h1>
+        <h3>${_.join(['Hello', 'World'], ' ')}
+    </div>
     `;
   return element;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,31 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/styles.css',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: './js/main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        use: 'babel-loader',
+        include: /^((?!\.test\.).)*\.js$/,
+      },
+      {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: true,
-            },
-          },
-        ],
-        include: /\.module\.css$/,
+        use: ['style-loader', 'css-loader'],
+        include: /^((?!\.module).)*\.css$/,
       },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        exclude: /\.module\.css$/,
+        include: /\.module\.css$/,
       },
       {
         test: /\.jpg$/,
