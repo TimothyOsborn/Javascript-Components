3984543ce0c4b77f80643a6005ac5ab9a472ad50
Timothy Tomazi-Osborn <timothytomaziosborn@gmail.com>
2020-04-19 20:34:33 +1000

    component(whitespace separator): added whitespace separator component

---
+++ src/components/Cards/index.ts
@@ -0,0 +1,42 @@
+import './card.module.scss';
+
+class CardElement extends HTMLElement {
+  onClick: string;
+  title: string;
+  static get observedAttributes() {
+    return ['title'];
+  }
+
+  constructor() {
+    super();
+    setTimeout(() => {
+      this.title = this.getAttribute('title');
+      this.onClick = this.getAttribute('action');
+      this.addEventListener('click', () => this.onClickHandler());
+    });
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    if (name === 'title' && oldValue !== null && oldValue !== newValue) {
+      this.connectedCallback();
+    }
+  }
+
+  onClickHandler() {
+    eval(this.onClick);
+  }
+
+  connectedCallback() {
+    setTimeout(() => {
+      this.innerHTML = `
+        <div class="container-button">
+          <div class="inner button-title">
+            ${this.title}
+          </div>
+        </div>
+        `;
+    });
+  }
+}
+
+customElements.define('ui-card', CardElement);
--- src/components/Layouts/Seperator/index.js
+++ src/components/Layouts/Seperator/index.js
@@ -11,5 +11,16 @@ class Seperator extends HTMLElement {
         });
     }
 }
+class WhitespaceSeperator extends HTMLElement {
+    connectedCallback() {
+        setTimeout(() => {
+            this.innerHTML = `
+      <div class="seperator whitespace">
+      </div>
+      `;
+        });
+    }
+}
 customElements.define('ui-seperator', Seperator);
+customElements.define('ui-seperator-whitespace', WhitespaceSeperator);
 //# sourceMappingURL=index.js.map
