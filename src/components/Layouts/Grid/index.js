1c04e15b8e3adcfb90ada61ca54ce21a688a0990
Timothy Tomazi-Osborn <timothytomaziosborn@gmail.com>
2020-04-23 08:42:32 +1000

    Merge Reso

--- src/components/Layouts/Grid/index.js
+++ src/components/Layouts/Grid/index.js
@@ -9,22 +9,7 @@ class FlexGrid extends HTMLElement {
         setTimeout(() => {
             this.innerHTML = `
         <div class="flex-grid">
-<<<<<<< HEAD
           ${this.innerHTML}
-=======
-          <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="fill">
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="fill">
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="fill">
-            </div>
-          </div>
->>>>>>> Added a little nonresponsive css to layout
         </div>
         `;
         });
--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,18 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Updated webpack to work with extracted css. TODO: Module specific CSS to be reconfigured correctly
   plugins: [
     new MiniCssExtractPlugin({
       filename: './css/styles.css',
     }),
   ],
-<<<<<<< HEAD
   output: {
     filename: './js/main.js',
-=======
-  plugins: [new MiniCssExtractPlugin()],
-  output: {
-    filename: 'main.js',
->>>>>>> Added initial webpack config and testing
-=======
-  output: {
-    filename: './js/main.js',
->>>>>>> Updated webpack to work with extracted css. TODO: Module specific CSS to be reconfigured correctly
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-<<<<<<< HEAD
-<<<<<<< HEAD
         test: /\.ts$/,
         use: 'babel-loader',
         include: /^((?!\.test\.).)*\.ts$/,
@@ -45,31 +29,6 @@ module.exports = {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         include: /\.module\.scss$/,
-=======
-=======
-        test: /\.js$/,
-        use: 'babel-loader',
-        include: /^((?!\.test\.).)*\.js$/,
-      },
-      {
->>>>>>> Updated webpack to work with extracted css. TODO: Module specific CSS to be reconfigured correctly
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        include: /^((?!\.module).)*\.css$/,
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-<<<<<<< HEAD
-        exclude: /\.module\.css$/,
->>>>>>> Added initial webpack config and testing
-=======
-        include: /\.module\.css$/,
->>>>>>> Updated webpack to work with extracted css. TODO: Module specific CSS to be reconfigured correctly
-      },
-      {
-        test: /\.jpg$/,
-        use: 'file-loader',
       },
     ],
   },
