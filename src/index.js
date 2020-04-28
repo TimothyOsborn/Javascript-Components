3690b2cfbe51654fcf66ae17dc653bd058ba58e9
Timothy Tomazi-Osborn <timothytomaziosborn@gmail.com>
2020-04-28 21:28:06 +1000

    component(image): bootstrap for initial image component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-require('./index.scss');
-require('./index.module.scss');
-require('./components/Banner/index');
-require('./components/Buttons/index');
-require('./components/Layouts/Grid/index');
-require('./components/Layouts/Seperator/index');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./index.scss");
+require("./index.module.scss");
+require("./components/Banner/index");
+require("./components/Buttons/index");
+require("./components/Layouts/Grid/index");
+require("./components/Layouts/Seperator/index");
+require("./components/Images/index");
 const homepage = () => {
-  const element = document.createElement('div');
-  element.innerHTML = `
+    const element = document.createElement('div');
+    element.innerHTML = `
       <button-custom title='Press Me' action='console.log("pineapple")'>
         Hello button
       </button-custom>
@@ -17,15 +18,15 @@ const homepage = () => {
       <ui-seperator></ui-seperator>
       <br>
       <grid-custom>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
       </grid-custom>
     `;
-  return element;
+    return element;
 };
 document.body.appendChild(homepage());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import './components/Banner/index';
 import './components/Buttons/index';
 import './components/Layouts/Grid/index';
 import './components/Layouts/Seperator/index';
+import './components/Images/index';
 
 const homepage = () => {
   const element = document.createElement('div');
@@ -16,12 +17,12 @@ const homepage = () => {
       <ui-seperator></ui-seperator>
       <br>
       <grid-custom>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
-        <div class="col-12-xs col-6-sm col-4-lg">blah</div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
       </grid-custom>
     `;
   return element;
