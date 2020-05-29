4a58909d06644d708495b41842bc4d244b3e94f2
Timothy Tomazi-Osborn <timothytomaziosborn@gmail.com>
2020-05-29 21:33:00 +1000

    chore(sample): updated index sample to include new components

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ require("./index.scss");
 require("./index.module.scss");
 require("./components/Banner/index");
 require("./components/Buttons/index");
+require("./components/Cards/index");
+require("./components/Images/index");
 require("./components/Layouts/Grid/index");
 require("./components/Layouts/Seperator/index");
-require("./components/Images/index");
 require("./components/Typography/index");
 const homepage = () => {
     const element = document.createElement('div');
+    element.setAttribute('class', 'body__background');
     element.innerHTML = `
       <button-custom title='Press Me' action='console.log("pineapple")'>
         Hello button
@@ -18,16 +20,36 @@ const homepage = () => {
       <ui-seperator-whitespace></ui-seperator-whitespace>
       <ui-seperator></ui-seperator>
       <br>
-      <grid-custom>
-        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
-        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
-        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+      <grid-custom height: >
+        <div class="col-12-xs col-6-sm col-4-lg">
+          <ui-card style="height:100%;">
+            <ui-image alt="blah" src="https://cdn4.vectorstock.com/i/1000x1000/54/48/color-triangular-abstract-background-vector-22655448.jpg"></ui-image>
+            <ui-typography>
+              <h3>Card Heading</h3>
+              <p>Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. 
+            </ui-typography>
+          </ui-card>
+        </div>
+        <div class="col-12-xs col-6-sm col-4-lg">
+          <ui-card>
+            <ui-image alt="blah" src="https://cdn.wallpapersafari.com/35/95/MaVqzY.jpg"></ui-image>
+            <ui-typography>
+              <h3>Card Heading</h3>
+              <p>Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. 
+            </ui-typography>
+          </ui-card>
+        </div>
+        <div class="col-12-xs col-6-sm col-4-lg"> 
+          <ui-card>
+            <ui-image alt="blah" src="https://static.vecteezy.com/system/resources/previews/000/101/236/non_2x/free-abstract-background-12-vector.jpg"></ui-image>
+            <ui-typography>
+                <h3>Card Heading</h3>
+                <p>Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. 
+            </ui-typography>
+          </ui-card>
+        </div>
       </grid-custom>
-      <ui-typography>
-        <p>P style, The quick brown fox jumps over the lazy dog.</p>
-        <div><p>P style, The quick brown fox jumps over the lazy dog.</p></div>
-      </ui-typography>
-    `;
+`;
     return element;
 };
 document.body.appendChild(homepage());
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import './index.scss';
 import './index.module.scss';
 import './components/Banner/index';
 import './components/Buttons/index';
+import './components/Cards/index';
+import './components/Images/index';
 import './components/Layouts/Grid/index';
 import './components/Layouts/Seperator/index';
-import './components/Images/index';
 import './components/Typography/index';
 
 const homepage = () => {
   const element = document.createElement('div');
+  element.setAttribute('class', 'body__background');
   element.innerHTML = `
       <button-custom title='Press Me' action='console.log("pineapple")'>
         Hello button
@@ -17,16 +19,36 @@ const homepage = () => {
       <ui-seperator-whitespace></ui-seperator-whitespace>
       <ui-seperator></ui-seperator>
       <br>
-      <grid-custom>
-        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
-        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
-        <div class="col-12-xs col-6-sm col-4-lg"><ui-image alt="blah" src="https://vignette.wikia.nocookie.net/mahousenseinegima/images/1/1a/Card_nodoka.jpg"></ui-images></div>
+      <grid-custom height: >
+        <div class="col-12-xs col-6-sm col-4-lg">
+          <ui-card style="height:100%;">
+            <ui-image alt="blah" src="https://cdn4.vectorstock.com/i/1000x1000/54/48/color-triangular-abstract-background-vector-22655448.jpg"></ui-image>
+            <ui-typography>
+              <h3>Card Heading</h3>
+              <p>Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. 
+            </ui-typography>
+          </ui-card>
+        </div>
+        <div class="col-12-xs col-6-sm col-4-lg">
+          <ui-card>
+            <ui-image alt="blah" src="https://cdn.wallpapersafari.com/35/95/MaVqzY.jpg"></ui-image>
+            <ui-typography>
+              <h3>Card Heading</h3>
+              <p>Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. 
+            </ui-typography>
+          </ui-card>
+        </div>
+        <div class="col-12-xs col-6-sm col-4-lg"> 
+          <ui-card>
+            <ui-image alt="blah" src="https://static.vecteezy.com/system/resources/previews/000/101/236/non_2x/free-abstract-background-12-vector.jpg"></ui-image>
+            <ui-typography>
+                <h3>Card Heading</h3>
+                <p>Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. Lorum Ipsom Dolor Sit. 
+            </ui-typography>
+          </ui-card>
+        </div>
       </grid-custom>
-      <ui-typography>
-        <p>P style, The quick brown fox jumps over the lazy dog.</p>
-        <div><p>P style, The quick brown fox jumps over the lazy dog.</p></div>
-      </ui-typography>
-    `;
+`;
   return element;
 };
 
