9512ef0c5241ea693b9fef223b562ce817cf4ecc
Timothy Tomazi-Osborn <timothytomaziosborn@gmail.com>
2021-03-06 21:03:20 +1100

    removing gitignore files

--- src/constants/constants/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let globals = {
-    SITE_NAME: 'Default Site Name',
-    SITE_TITLE: 'Default Site Title',
-};
-exports.default = globals;
-//# sourceMappingURL=constants.js.map
